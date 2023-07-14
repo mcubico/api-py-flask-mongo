e128c8c7c8a72c448c47b71509bcac9607cfc0bd
Mauricio Montoya Medrano <mcubico33@gmail.com>
2023-07-14 10:20:29 -0500

    Agrego lógica para escoger las configuraciones dependiendo del ambiente

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 from flask import Flask
 
@@ -6,7 +8,12 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     # the path to your .env file (or any other file of environment variables you want to load)
     load_dotenv('.env')
-    app.config.from_pyfile('../settings.py')
+    is_production_env = os.environ.get('FLASK_ENV') == 'production'
+    settings_path = '../settings.py'
+    if is_production_env:
+        settings_path = 'settings.py'
+
+    app.config.from_pyfile(settings_path)
 
     if __name__ == '__main__':
         app.run(debug=True)
